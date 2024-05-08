a0a53b37843bb43907ab6952696a26d1f9648397
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-08 12:23:54 +0530

    git commit

--- 4-Logical-Operations/LogicalOperations.s
+++ 4-Logical-Operations/LogicalOperations.s
@@ -10,3 +10,7 @@
     EOR R5, R1, R2         ; Perform XOR between R1 and R2 and store in R5
     MVN R6, R1             ; Invert all bits of R1 and store in R6
 
+// AND R3, R1, #0xF0 clears the lower nibble of R1 (1010xxxx) and stores the result in R3
+// ORR R4, R2, #0x0F sets the lower nibble of R2 (xxxx1100) to 1111 and stores the result in R4
+// EOR R5, R1, R2 performs an XOR between R1 (10101010) and R2 (11001100) and stores the result in R5
+// MVN R6, R1 inverts all bits of R1 (10101010) and stores the result in R6
